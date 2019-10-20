608df302150062f3f7c584110e8ffe0395ea57f0
Liviu Preda <liviupreda@gmail.com>
2019-10-20 09:06:20 +0300

    signin& register routes; seed database

--- server.js
+++ server.js
@@ -1,10 +1,59 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = 3000;
-
 const app = express();
 
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
+
 app.get('/', (req, res) => {
-  res.send('Running ...');
+  res.send(database.users);
+});
+
+// SIGN IN
+app.post('/signin', (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json('OK Login');
+  } else {
+    res.status(400).json('ERR Login');
+  }
+});
+
+// CREATE NEW USER
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
 });
 
 app.listen(port, () => {
